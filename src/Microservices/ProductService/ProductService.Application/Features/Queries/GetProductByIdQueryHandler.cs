947b98b5442ca71ad39976045069a917043f3dcb
hakannyucel <hakanyucelsw@gmail.com>
2023-02-10 22:38:37 +0300

    Add Application Project

---
+++ src/Microservices/ProductService/ProductService.Application/Features/Queries/GetProductByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Common.Models;
+using Common.RequestModels.ProductService;
+using Common.ResponseModels;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductService.Application.Features.Queries
+{
+    public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdRequestModel, ApiResponse<GetProductByIdResponseModel>>
+    {
+        public async Task<ApiResponse<GetProductByIdResponseModel>> Handle(GetProductByIdRequestModel request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
