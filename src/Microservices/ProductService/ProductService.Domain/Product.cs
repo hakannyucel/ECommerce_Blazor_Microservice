4e18e5f7aa5d378244af447082a5c673966c0b62
hakannyucel <hakanyucelsw@gmail.com>
2023-02-10 22:26:12 +0300

    Update Product Model
    Updated model for client app

--- src/Microservices/ProductService/ProductService.Domain/Product.cs
+++ src/Microservices/ProductService/ProductService.Domain/Product.cs
@@ -5,5 +5,10 @@ namespace ProductService.Domain
     public class Product : Entity
     {
         public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public int ReviewCount { get; set; }
+        public List<string> Images { get; set; }
     }
 }
