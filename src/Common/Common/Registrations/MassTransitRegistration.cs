9c9ad4af62d4223d2c4f17562a01341efc70ee09
hakannyucel <hakanyucelsw@gmail.com>
2023-04-12 18:52:28 +0300

    Add MassTransit Registration

---
+++ src/Common/Common/Registrations/MassTransitRegistration.cs
@@ -0,0 +1,30 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Common.Registrations
+{
+    public static class MassTransitRegistration
+    {
+        public static IServiceCollection AddMassTransitEventBus(this IServiceCollection services, IConfiguration configuration, Assembly assembly)
+        {
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumers(assembly);
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(configuration["RabbitMQ:Host"], "/", h =>
+                    {
+                        h.Username(configuration["RabbitMQ:Username"]);
+                        h.Password(configuration["RabbitMQ:Password"]);
+                    });
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
+            services.AddMassTransitHostedService();
+
+            return services;
+        }
+    }
+}
