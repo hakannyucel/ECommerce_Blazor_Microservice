f30cb8397369b042402baa1e7b9dc2ccafe5fad1
hakannyucel <hakanyucelsw@gmail.com>
2023-04-12 19:04:50 +0300

    Add [GET] GetCatalogItemList Endpoint

---
+++ src/Microservices/CatalogService/CatalogService.WebApi/Controllers/CatalogItemController.cs
@@ -0,0 +1,29 @@
+﻿using CatalogService.WebApi.Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogService.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogItemController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CatalogItemController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("GetCatalogItemList")]
+        public async Task<IActionResult> GetCatalogItemList()
+        {
+            var result = await _mediator.Send(new GetCatalogListQuery());
+
+            if (!result.IsSuccess) 
+                return BadRequest(result);
+
+            return Ok(result);
+        }
+    }
+}
